4ed422ba3cd09ba71e3aba352331d3ecd1cd3c73
Jay Patel <jay2jaykp@gmail.com>
2021-08-19 02:28:38 -0400

    resolving axios baseurl

--- client/src/services/api/index.ts
+++ client/src/services/api/index.ts
@@ -1,5 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const api = axios.create({
-  baseURL: 'http://localhost:5000/todo',
-});
+export const api = axios.create(
+  {
+    baseURL:
+      process.env.NODE_ENV === "production"
+        ? `${window.location.origin}/todo`
+        : "http://localhost:5555/todo",
+  }
+  // baseURL: 'http://localhost:5000/todo',
+  // }
+);
